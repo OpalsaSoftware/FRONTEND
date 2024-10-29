e835f03f1697944e77f62745c51d3e7fcd7e5d06
OPALSASASSOFTWARE <opalsasassoftware@gmail.com>
2024-10-29 03:52:41 -0500

    borrar funcion de verificar token

--- src/api/axios.js
+++ src/api/axios.js
@@ -6,20 +6,6 @@ const instance = axios.create({
   withCredentials: true // Permitir que Axios envíe cookies con las solicitudes
 });
 
-// Interceptor para añadir el token en cada solicitud
-instance.interceptors.request.use((config) => {
-  const token = document.cookie
-    .split('; ')
-    .find(row => row.startsWith('rw.authenticated='))
-    ?.split('=')[1]; // Extrae el token de la cookie
-
-  if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`; // Añade el token a los headers
-  }
-
-  return config; // Devuelve la configuración modificada
-}, (error) => {
-  return Promise.reject(error); // Rechaza el error
-});
+// Interceptor para añadir el token en cada solicitu
 
 export default instance;
